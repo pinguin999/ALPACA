fdcd64c94026fd1842701cc67abd6404ff8581a4
Kolja Lubitz <pinguin999@gmail.com>
2024-11-18 22:11:35 +0100

    Add gmae.old_scene to Lua state

--- src/scene.cpp
+++ src/scene.cpp
@@ -39,6 +39,7 @@ Scene::Scene(const std::string &fileName, const std::shared_ptr<Game> &game) : f
         old_scene = (*game->lua_state)["game"]["scene"];
     }
     (*game->lua_state).script("game.scene = '" + fileName + "'");
+    (*game->lua_state).script("game.old_scene = '" + old_scene + "'");
 
     if (!(*game->lua_state)["scenes"].valid())
     {
