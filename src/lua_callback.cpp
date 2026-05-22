c171f6d5f18e1d2bf8496aec0966b79f6266f988
Carsten Pfeffer <pfeffer.carsten@gmail.com>
2026-05-22 10:55:29 +0200

    also logging errors in Lua callbacks

--- src/lua_callback.cpp
+++ src/lua_callback.cpp
@@ -1,6 +1,7 @@
 #include "lua_callback.hpp"
 
 #include "spine_object.hpp"
+#include <sol/protected_function_result.hpp>
 
 LuaCallback::LuaCallback(sol::function callback, std::shared_ptr<sol::state> lua_state)
 : callback(std::move(callback)), lua_state(std::move(lua_state)),
@@ -13,7 +14,12 @@ bool LuaCallback::calls_function(const sol::function& function) {
 
 void LuaCallback::operator()() {
 	lua_state->set("this", thisObject);
-	callback();
+	sol::protected_function_result result = callback();
+	if(!result.valid())
+	{
+		const sol::error err = result;
+		jngl::error("Error in Lua callback: {}", err.what());
+	}
 }
 
 bool operator==(const LuaCallback& a, const LuaCallback& b) {
